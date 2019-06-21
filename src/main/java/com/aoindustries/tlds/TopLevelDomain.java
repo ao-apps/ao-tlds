fd9c340b41d3fca40a7a06cfef2fa18636b981a0
AO Industries, Inc. <support@aoindustries.com>
2019-06-21 03:19:19 -0500

    Updated bundled tlds-alpha-by-domain.txt to version 2019061901.

--- src/main/java/com/aoindustries/tlds/TopLevelDomain.java
+++ src/main/java/com/aoindustries/tlds/TopLevelDomain.java
@@ -64,9 +64,9 @@ public class TopLevelDomain {
 	/**
 	 * Hard-coded bootstrap data.  This is used when no download completed or possible.
 	 * This matches the bundled copy of tlds-alpha-by-domain.txt
-	 * <pre>date +%s000L -d "Mon Apr 22 07:07:01 2019 UTC"</pre>
+	 * <pre>date +%s000L -d "Thu Jun 20 07:07:01 2019 UTC"</pre>
 	 */
-	private static final long LAST_UPDATED = 1555916821000L;
+	private static final long LAST_UPDATED = 1561014421000L;
 
 	/**
 	 * In DEBUG mode, times are greatly shortened for testing.
