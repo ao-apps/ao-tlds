6a2d29bb9d85769eeda8b19df48cbf289a0dee2f
AO Industries, Inc <support@aoindustries.com>
2020-09-03 05:55:08 -0500

    Resolved editor warnings

--- src/test/java/com/aoindustries/tlds/TopLevelDomainTest.java
+++ src/test/java/com/aoindustries/tlds/TopLevelDomainTest.java
@@ -1,7 +1,24 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * ao-tlds - Self-updating Java API to get top-level domains.
+ * Copyright (C) 2016, 2020  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-tlds.
+ *
+ * ao-tlds is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-tlds is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-tlds.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.aoindustries.tlds;
 
@@ -9,6 +26,7 @@ import java.util.Date;
 import java.util.List;
 import static org.junit.Assert.assertTrue;
 
+@SuppressWarnings("UseOfSystemOutOrSystemErr")
 public class TopLevelDomainTest {
 	
 	public TopLevelDomainTest() {
