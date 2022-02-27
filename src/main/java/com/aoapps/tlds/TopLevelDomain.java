22f6b03b7e37c3972ec00c1ac3465574fd1f6a33
AO Industries, Inc <support@aoindustries.com>
2022-02-27 00:43:38 -0600

    Using final instead of abstract for static utility classes
    NetBeans 12.6 is warning "Constructor is never used" when abstract, and
    this cannot be suppressed with `@SuppressWarnings("unused")`.

--- src/main/java/com/aoapps/tlds/TopLevelDomain.java
+++ src/main/java/com/aoapps/tlds/TopLevelDomain.java
@@ -1,6 +1,6 @@
 /*
  * ao-tlds - Self-updating Java API to get top-level domains.
- * Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2017, 2018, 2019, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -62,7 +62,7 @@ import java.util.prefs.Preferences;
  *
  * @author  AO Industries, Inc.
  */
-public abstract class TopLevelDomain {
+public final class TopLevelDomain {
 
 	/** Make no instances. */
 	private TopLevelDomain() {throw new AssertionError();}
