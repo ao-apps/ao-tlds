2b59cbec2f95ca59b8890ab6084b1e5cf3e8c7be
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:34:30 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/tlds/TopLevelDomain.java
+++ src/main/java/com/aoapps/tlds/TopLevelDomain.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-tlds.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.tlds;
 
 import com.aoapps.collections.AoCollections;
--- src/test/java/com/aoapps/tlds/TopLevelDomainTest.java
+++ src/test/java/com/aoapps/tlds/TopLevelDomainTest.java
@@ -1,6 +1,6 @@
 /*
  * ao-tlds - Self-updating Java API to get top-level domains.
- * Copyright (C) 2016, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2016, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-tlds.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.tlds;
 
 import java.util.Date;
